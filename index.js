52ec8aba23cdbd7ec3a0a7e2485ce0d4e68c5be2
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-27 17:23:11 -0500

    building index

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
-
+const port = 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("hello")
+    // res.send("hello")
+    const data = {
+        name: Jashan,
+        isAwesome: true
+    }
+    res.json(data);
+})
+
+app.get('/awesome-generator', (req, res) => {
+    const {name, isAwesome} = req.query;
+    res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not '} awesome`)
+})
+
+app.listen(port, () => {
+    console.log(`Express app runnning at ${port}`)
 })
