12b6ba6fc9d2403a942ffde1df07abcf1e6e1179
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-29 09:43:46 -0500

    adding app

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
     // res.send("hello")
     const data = {
-        name: Jashan,
+        name: 'Jashan',
         isAwesome: true
     }
     res.json(data);
