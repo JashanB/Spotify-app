6c0a8bf8d615d94ca3cfba7b2ecee440452bf325
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-03 18:04:58 -0500

    cors issue still

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,22 @@ import { useEffect } from 'react';
 function App() {
 
   useEffect(() => {
-    const querystring = window.location.search;
-    const urlParams = new URLSearchParams(querystring);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
     const accessToken = urlParams.get('access_token');
     const refreshToken = urlParams.get('refresh_token');
+
+    console.log(accessToken);
+    console.log(refreshToken);
+
     if (refreshToken) {
-      fetch(`http://localhost:8000/refresh_token?refresh_token=
-      ${refreshToken}`)
+      fetch(`http://localhost:8000/refresh_token?refresh_token=${refreshToken}`)
         .then(res => res.json())
         .then(data => console.log(data))
-        .catch(err => console.error(err))
+        .catch(err => console.error(err));
     }
-  }, [])
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
