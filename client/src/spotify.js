a35ee954e289ac4189cfd8b0b61aa5784bdd52fc
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-04 12:05:40 -0500

    saving values to local storage

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -18,7 +18,32 @@ const getAccessToken = () => {
     const accessToken = urlParams.get('access_token');
     const refreshToken = urlParams.get('refresh_token');
 
-    return accessToken;
+    const hasError = urlParams.get('error');
+
+    // If there's an error OR the token in localStorage has expired, refresh the token
+    if (hasError || hasTokenExpired() || LOCALSTORAGE_VALUES.accessToken === 'undefined') {
+      refreshToken();
+    }
+  
+    // If there is a valid access token in localStorage, use that
+    if (LOCALSTORAGE_VALUES.accessToken && LOCALSTORAGE_VALUES.accessToken !== 'undefined') {
+      return LOCALSTORAGE_VALUES.accessToken;
+    }
+  
+    // If there is a token in the URL query params, user is logging in for the first time
+    if (queryParams[LOCALSTORAGE_KEYS.accessToken]) {
+      // Store the query params in localStorage
+      for (const property in queryParams) {
+        window.localStorage.setItem(property, queryParams[property]);
+      }
+      // Set timestamp
+      window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+      // Return access token from query params
+      return queryParams[LOCALSTORAGE_KEYS.accessToken];
+    }
+  
+    return false;
+    // return accessToken;
 
     // console.log(accessToken);
     // console.log(refreshToken);
