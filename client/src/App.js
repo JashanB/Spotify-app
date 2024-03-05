3f494d95619c28fba3d4b76ba02ccc7c0265bb73
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-05 12:10:37 -0500

    adding jsx for app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import { access_token, logout, getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
 
@@ -22,30 +27,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {!token ? (
-          <a
-            className="App-link"
-            href="http://localhost:8000/login"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Log in to spotify
+      {!token ? (
+          <a className="App-link" href="http://localhost:8000/login">
+            Log in to Spotify
           </a>
         ) : (
-          <div>
-            <h1>Logged in!</h1>
-            <button onClick={logout}>Log out</button>
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>Followers: {profile.followers.total}</p>
-                {profile.images.length > 0 && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="avatar"></img>
-                )
-                }
-              </div>
-            )}
-          </div>
+          <Router>
+            <Switch>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/playlists/:id">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/playlists">
+                <h1>Playlists</h1>
+              </Route>
+              <Route path="/">
+                <>
+                  <button onClick={logout}>Log Out</button>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )}
+                </>
+              </Route>
+            </Switch>
+          </Router>
         )}
       </header>
     </div>
