406f6859cb5850a131e5af0008daecb7daf8859a
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-05 19:39:15 -0500

    cleanup app

--- client/src/App.js
+++ client/src/App.js
@@ -44,19 +44,7 @@ function App() {
               <Route path="/top-tracks" element={<TopTracks />}/>
               <Route path="/playlists/:id" element={<Playlist />}/>
               <Route path="/playlists" element={<Playlists />}/>
-              <Route path="/" element={<Login />}/>
-                {/* <>
-                  <button onClick={logout}>Log Out</button>
-                  {profile && (
-                    <div>
-                      <h1>{profile.display_name}</h1>
-                      <p>{profile.followers.total} Followers</p>
-                      {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="Avatar"/>
-                      )}
-                    </div>
-                  )}
-                </> */}
+              <Route path="/" element={<Login profile={profile}/>}/>
             </Routes>
           </Router>
         )}
