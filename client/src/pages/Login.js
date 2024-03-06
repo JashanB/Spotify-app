09f1fa6e1b5126b236c81aa0e006345b64f0df5a
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-06 13:09:18 -0500

    moved login landing page logic to Login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,32 @@
 import { logout } from '../spotify';
+import styled from 'styled-components/macro';
 
-export default function Login({ profile }) {
+export default function Login(props) {
+
+    const StyledLoginButton = styled.a`
+    background-color:green;
+    color: white;
+    padding: 10px 20px;
+    margin: 20px auto;
+    border-radius: 30px;
+    display: inline-block;
+    `
 
     return (
         <div>
-            <button onClick={logout}>Log Out</button>
-            {profile && (
+            {!props.token ? (
+                <StyledLoginButton href="/login">Log in to Spotify</StyledLoginButton>
+            ) : (
                 <div>
-                    <h1>{profile.display_name}</h1>
-                    <p>{profile.followers.total} Followers</p>
-                    {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="Avatar" />
+                    <button onClick={logout}>Log Out</button>
+                    {props.profile && (
+                        <div>
+                            <h1>{props.profile.display_name}</h1>
+                            <p>{props.profile.followers.total} Followers</p>
+                            {props.profile.images.length && props.profile.images[0].url && (
+                                <img src={props.profile.images[0].url} alt="Avatar" />
+                            )}
+                        </div>
                     )}
                 </div>
             )}
