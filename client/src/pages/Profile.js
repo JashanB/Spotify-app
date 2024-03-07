a96878c715c3046a843291af2d7f0b323105091d
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-07 16:05:21 -0500

    profile mock

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,31 +2,42 @@ import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
 import { getCurrentUserProfile } from '../spotify';
 
-const Profile = ({profile}) => {
-//   const [profile, setProfile] = useState(null);
+const Profile = ({ profile }) => {
+    //   const [profile, setProfile] = useState(null);
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const { data } = await getCurrentUserProfile();
-//       setProfile(state => data);
-//     };
+    //   useEffect(() => {
+    //     const fetchData = async () => {
+    //       const { data } = await getCurrentUserProfile();
+    //       setProfile(state => data);
+    //     };
 
-//     catchErrors(fetchData());
-//   }, []);
+    //     catchErrors(fetchData());
+    //   }, []);
 
-  return (
-    <>
-      {profile && (
-        <div>
-          <h1>{profile.display_name}</h1>
-          <p>{profile.followers.total} Followers</p>
-          {profile.images.length && profile.images[0].url && (
-            <img src={profile.images[0].url} alt="Avatar"/>
-          )}
-        </div>
-      )}
-    </>
-  )
+    return (
+        <>
+            {profile && (
+                <div className='container'>
+                    <div className='picture'> {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar" />
+                    )}</div>
+                    <div className='info'>
+                        <h1>{profile.display_name}</h1>
+                        <p>{profile.followers.total} Followers</p>
+                    </div>
+                </div>
+
+
+                // <div>
+                //   <h1>{profile.display_name}</h1>
+                //   <p>{profile.followers.total} Followers</p>
+                //   {profile.images.length && profile.images[0].url && (
+                //     <img src={profile.images[0].url} alt="Avatar"/>
+                //   )}
+                // </div>
+            )}
+        </>
+    )
 };
 
 export default Profile;
