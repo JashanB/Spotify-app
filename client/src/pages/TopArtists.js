2fbc81a343f4a4686271e79a6ba5dc1f71416288
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-09 16:30:23 -0500

    adding top artists page

--- client/src/pages/TopArtists.js
+++ client/src/pages/TopArtists.js
@@ -2,12 +2,17 @@ import { SectionWrapper, ArtistsGrid } from "../components"
 
 export default function TopArtists({ topArtists }) {
     return (
-        <>
+        <main>
+            <ul>
+                <li><button onClick={}>This Month</button></li>
+                <li><button onClick={}>Last 6 Months</button></li>
+                <li><button onClick={}>All Time</button></li>
+            </ul>
             {topArtists && (
-                <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
+                <SectionWrapper title="Top Artists" breadcrumb="true">
                     <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
                 </SectionWrapper>
             )}
-        </>
+        </main>
     )
 }
