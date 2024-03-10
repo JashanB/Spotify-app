33868369b136d35b9a4cb2f19a2e0f3a6245f2ea
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-10 16:31:36 -0400

    added top artiists object

--- client/src/pages/TopArtists.js
+++ client/src/pages/TopArtists.js
@@ -1,6 +1,7 @@
 import { SectionWrapper, ArtistsGrid } from "../components"
 import { useState, useEffect } from "react"
 import { getTopArtists } from "../spotify";
+import { catchErrors } from '../utils';
 
 export default function TopArtists({ topArtists }) {
     const [activeRage, setActiveRange] = useState(null);
@@ -12,24 +13,37 @@ export default function TopArtists({ topArtists }) {
 
     useEffect(() => {
         if (topArtists && topArtists.items && topArtists.items.length > 1) {
+            // console.log('setting top artists')
             setArtistsObj(state => ({
                 ...state,
                 'short_term': topArtists.items
             }))
         }
-        console.log(artistsObj)
         async function fetchArtists() {
             const mediumTermTopArtists = await getTopArtists('medium_term');
-            const longermTopArtists = await getTopArtists('long_term');
-            // setTopArtists(state => usersTopArtists.data);
+            const longTermTopArtists = await getTopArtists('long_term');
+            // console.log(mediumTermTopArtists)
+            // console.log(longTermTopArtists)
+            setArtistsObj(state => ({
+                ...state,
+                'medium_term': mediumTermTopArtists.data.items,
+                'long_term': longTermTopArtists.data.items
+            }))
+            // console.log('setting artists obj')
+            // console.log({artistsObj})
 
         }
+        if (topArtists) {
+            // console.log('calling fetch art')
+            catchErrors(fetchArtists())
+        };
     }, [])
     //currently fetching short range artists
     //want to fetch medium and long term 
     //can fetch all in background, or fetch when clicked
     //set active range array to artists when needed
     //make obj with artists 
+    console.log(artistsObj)
 
     return (
         <main>
