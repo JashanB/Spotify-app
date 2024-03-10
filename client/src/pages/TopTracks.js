74d9be122b0f1a054337d218d7ce26ba7808e93b
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-10 19:19:31 -0400

    adding top tracks

--- client/src/pages/TopTracks.js
+++ client/src/pages/TopTracks.js
@@ -1,7 +1,51 @@
-export default function TopTracks () {
-    return (
-        <div>
+import { SectionWrapper, TrackList, TimeRangeButtons } from "../components"
+import { useState, useEffect } from "react"
+import { getTopTracks } from "../spotify";
+import { catchErrors } from '../utils';
 
-        </div>
+export default function TopTracks({ topTracks }) {
+    const [activeRange, setActiveRange] = useState(null);
+    const [activestate, setActiveState] = useState('short_term');
+    const [tracksObj, setTracksObj] = useState({
+        'short_term': [],
+        'medium_term': [],
+        'long_term': []
+    });
+
+    useEffect(() => {
+        if (topTracks && topTracks.items && topTracks.items.length > 1) {
+            setActiveRange(state => topTracks.items);
+            setTracksObj(state => ({
+                ...state,
+                'short_term': topTracks.items
+            }))
+        }
+        async function fetchArtists() {
+            const mediumTermTopTracks = await getTopTracks('medium_term');
+            const longTermTopTracks = await getTopTracks('long_term');
+            setTracksObj(state => ({
+                ...state,
+                'medium_term': mediumTermTopTracks.data.items,
+                'long_term': longTermTopTracks.data.items
+            }))
+        }
+        if (topTracks) { catchErrors(fetchArtists()) };
+    }, [topTracks])
+
+    console.log(tracksObj)
+    return (
+        <main>
+            {topTracks && (
+                <SectionWrapper title="Top Tracks" breadcrumb="true">
+                    <TimeRangeButtons
+                        activestate={activestate}
+                        setActiveState={setActiveState}
+                        setActiveRange={setActiveRange}
+                        tracksObj={tracksObj}
+                    />
+                    <TrackList tracks={activeRange && activeRange.slice(0, 10)} />
+                </SectionWrapper>
+            )}
+        </main>
     )
 }
