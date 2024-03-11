949632485458af5a775cf9d7132263c6c4d8a499
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-11 12:29:14 -0400

    adding playlist page

--- client/src/pages/Playlist.js
+++ client/src/pages/Playlist.js
@@ -1,7 +1,15 @@
-export default function Playlist () {
-    return (
-        <div>
+import { SectionWrapper } from "../components"
+import { getPlaylistById } from "../spotify"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { StyledHeader } from "../styles"
 
-        </div>
+export default function Playlist() {
+    const { id } = useParams();;
+
+    return (
+        <>
+
+        </>
     )
 }
