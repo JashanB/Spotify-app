6190150f6d80f7707ed0e589cc0d33edf00de995
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-10 20:33:30 -0400

    playlist data not updating

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -3,24 +3,29 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 import { catchErrors } from '../utils';
 
-export default function Playlists({ playlists }) {
+export default function Playlists({ playlists, setPlaylists }) {
     const [playlistArray, setPlaylistArray] = useState([]);
 
-    console.log(playlists)
     useEffect(() => {
         if (playlists) {setPlaylistArray(state => playlists.items)};
     }, [playlists]);
+    if (playlists) {console.log('next', playlists.next)}
 
     useEffect(() => {
         if (!playlists) { return }
         const fetchMoreData = async () => {
             if (playlists.next) {
+                console.log('next', playlists.next)
                 const {data} = await axios.get(playlists.next);
-                console.log(data)
-                setPlaylistArray(state => [...state, data], )
+                setPlaylists(state => data);
+                setPlaylistArray(state => [...state, ...playlists.items], )
+
             }
         }
+        catchErrors(fetchMoreData());
     }, [playlists])
+    console.log(playlistArray)
+
     return (
         <main>
             <SectionWrapper title="Public Playlists" breadcrumb={true}>
