1ef4e404c9578b23e358f9abca703cdc35b46553
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-11 12:33:29 -0400

    added header to playlist page

--- client/src/pages/Playlist.js
+++ client/src/pages/Playlist.js
@@ -15,9 +15,31 @@ export default function Playlist() {
             setPlaylist(state => data);
         }
         catchErrors(fetchData());
-    })
+    }, [id])
+
     return (
         <>
+            {playlist && (
+                <>
+                    <StyledHeader>
+                        <div className="header__inner">
+                            {playlist.images.length && playlist.images[0].url && (
+                                <img className="header__img" src={playlist.images[0].url} alt="Playlist Artwork" />
+                            )}
+                            <div>
+                                <div className="header__overline">Playlist</div>
+                                <h1 className="header__name">{playlist.name}</h1>
+                                <p className="header__meta">
+                                    {playlist.followers.total ? (
+                                        <span>{playlist.followers.total} {`follower${playlist.followers.total !== 1 ? 's' : ''}`}</span>
+                                    ) : null}
+                                    <span>{playlist.tracks.total} {`song${playlist.tracks.total !== 1 ? 's' : ''}`}</span>
+                                </p>
+                            </div>
+                        </div>
+                    </StyledHeader>
+                </>
+            )}
 
         </>
     )
