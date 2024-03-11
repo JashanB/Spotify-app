bb63b69b6e0de77242b86eebb35829c3be5df74a
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-11 12:31:10 -0400

    adding playlist page

--- client/src/pages/Playlist.js
+++ client/src/pages/Playlist.js
@@ -3,10 +3,19 @@ import { getPlaylistById } from "../spotify"
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { StyledHeader } from "../styles"
+import { catchErrors } from "../utils"
 
 export default function Playlist() {
     const { id } = useParams();;
+    const [playlist, setPlaylist] = useState(null);
 
+    useEffect(() => {
+        async function fetchData() {
+            const { data } = await getPlaylistById(id);
+            setPlaylist(state => data);
+        }
+        catchErrors(fetchData());
+    })
     return (
         <>
 
