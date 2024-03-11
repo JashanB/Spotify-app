bca3a02b05bbc4e68c6119120a87d39c09263765
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-11 13:08:36 -0400

    adding playlists jsx and fetch data functions

--- client/src/pages/Playlist.js
+++ client/src/pages/Playlist.js
@@ -4,43 +4,78 @@ import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { StyledHeader } from "../styles"
 import { catchErrors } from "../utils"
+import axios from "axios";
 
 export default function Playlist() {
-    const { id } = useParams();;
+    const { id } = useParams();
     const [playlist, setPlaylist] = useState(null);
-
+    const [tracksData, setTracksData] = useState(null);
+    const [tracks, setTracks] = useState(null);
+  
     useEffect(() => {
-        async function fetchData() {
-            const { data } = await getPlaylistById(id);
-            setPlaylist(state => data);
+      const fetchData = async () => {
+        const { data } = await getPlaylistById(id);
+        setPlaylist(data);
+        setTracksData(data.tracks);
+      };
+  
+      catchErrors(fetchData());
+    }, [id]);
+  
+    // When tracksData updates, compile arrays of tracks and audioFeatures
+    useEffect(() => {
+      if (!tracksData) {
+        return;
+      }
+  
+      // When tracksData updates, check if there are more tracks to fetch
+      // then update the state variable
+      const fetchMoreData = async () => {
+        if (tracksData.next) {
+          const { data } = await axios.get(tracksData.next);
+          setTracksData(data);
         }
-        catchErrors(fetchData());
-    }, [id])
-
+      };
+  
+      setTracks(tracks => ([
+        ...tracks ? tracks : [],
+        ...tracksData.items
+      ]));
+  
+      catchErrors(fetchMoreData());
+    }, [tracksData]);
+  
     return (
-        <>
-            {playlist && (
-                <>
-                    <StyledHeader>
-                        <div className="header__inner">
-                            {playlist.images.length && playlist.images[0].url && (
-                                <img className="header__img" src={playlist.images[0].url} alt="Playlist Artwork" />
-                            )}
-                            <div>
-                                <div className="header__overline">Playlist</div>
-                                <h1 className="header__name">{playlist.name}</h1>
-                                <p className="header__meta">
-                                    {playlist.followers.total ? (
-                                        <span>{playlist.followers.total} {`follower${playlist.followers.total !== 1 ? 's' : ''}`}</span>
-                                    ) : null}
-                                    <span>{playlist.tracks.total} {`song${playlist.tracks.total !== 1 ? 's' : ''}`}</span>
-                                </p>
-                            </div>
-                        </div>
-                    </StyledHeader>
-                </>
-            )}
-
-        </>
+      <>
+        {playlist && (
+          <>
+            <StyledHeader>
+              <div className="header__inner">
+                {playlist.images.length && playlist.images[0].url && (
+                  <img className="header__img" src={playlist.images[0].url} alt="Playlist Artwork"/>
+                )}
+                <div>
+                  <div className="header__overline">Playlist</div>
+                  <h1 className="header__name">{playlist.name}</h1>
+                  <p className="header__meta">
+                    {playlist.followers.total ? (
+                      <span>{playlist.followers.total} {`follower${playlist.followers.total !== 1 ? 's' : ''}`}</span>
+                    ) : null}
+                    <span>{playlist.tracks.total} {`song${playlist.tracks.total !== 1 ? 's' : ''}`}</span>
+                  </p>
+                </div>
+              </div>
+            </StyledHeader>
+  
+            <main>
+              <SectionWrapper title="Playlist" breadcrumb={true}>
+                {tracks && (
+                  <TrackList tracks={tracks} />
+                )}
+              </SectionWrapper>
+            </main>
+          </>
+        )}
+      </>
     )
 }
