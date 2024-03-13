54f3ed465cb723fb5ecd307aeeb9c75d3a90d4c4
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-13 14:52:02 -0400

    modifying login uri

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -29,9 +29,8 @@ const LOGIN_URI =
 process.env.NODE_ENV !== 'production'
   ? 'http://localhost:8000/login'
   : ' https://spotify-app-e9490d6b3f02.herokuapp.com/login';
-  
-export default function Login(props) {
 
+export default function Login(props) {
   return (
     <>
       {!props.token && (
