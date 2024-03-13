7838e0f5568e98003233e4fac2b51e362833d013
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-13 13:57:55 -0400

    updated login uri to heroku domain

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -29,7 +29,7 @@ const StyledLoginButton = styled(Link)`
 const LOGIN_URI =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:8000/login'
-    : 'https://spotify-app.herokuapp.com/login';
+    : ' https://spotify-app-e9490d6b3f02.herokuapp.com/login';
 
 
 export default function Login(props) {
